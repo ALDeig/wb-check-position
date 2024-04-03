29e036bf85c28df985528fc269c959d23287ec90
aldeig <alkin.denis@gmail.com>
2024-04-03 12:18:08 +0400

    fix: logging

--- app/settings.py
+++ app/settings.py
@@ -12,12 +12,13 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_DB: str
+    POSTGRES_PORT: int
 
     @property
     def DATABASE_URL(self) -> str:
         return (
             f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
-            f"@{self.POSTGRES_HOST}:5432/{self.POSTGRES_DB}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
         )
 
 
@@ -29,20 +30,25 @@ LOGGING_CONFIG = {
     "disable_existing_loggers": False,
     "formatters": {
         "default_formatter": {
-            "format": "[%(asctime)s] [%(levelname)7s] [%(name)s] > %(message)s",
+            "format": "[%(asctime)s] [%(levelname)-7s] [%(name)s] > %(message)s",
             "datefmt": "%Y-%m-%d %H:%M:%S",
         }
     },
     "handlers": {
         "stream_handler": {
             "class": "logging.StreamHandler",
-            "formater": "default_formatter",
+            "formatter": "default_formatter",
         }
     },
     "loggers": {
         "root": {
             "handlers": ["stream_handler"],
             "level": settings.LOG_LEVEL,
+            "propagate": True,
+        },
+        "httpx": {
+            "handlers": ["stream_handler"],
+            "level": "ERROR",
             "propagate": True,
         }
     },
