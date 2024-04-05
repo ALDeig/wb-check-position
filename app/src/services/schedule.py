225cdcedad1477d9b6478a42f72477def9b68f97
aldeig <alkin.denis@gmail.com>
2024-04-05 14:29:49 +0400

    fix: right time to tracking

--- app/src/services/schedule.py
+++ app/src/services/schedule.py
@@ -4,9 +4,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from app.src.services.tracking.mailing import mailing_notices
 from app.src.services.tracking.tracking import update_tracks
 
-scheduler = AsyncIOScheduler(timezone="Europe/Samara")
+scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 
 
 def add_jobs(bot: Bot):
-    scheduler.add_job(update_tracks, "cron", hour=19, minute=55)
-    scheduler.add_job(mailing_notices, "cron", hour=19, minute=57, args=[bot])
+    scheduler.add_job(update_tracks, "cron", hour=7)
+    scheduler.add_job(mailing_notices, "cron", hour=9, args=[bot])
