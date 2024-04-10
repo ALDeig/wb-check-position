e50971435ecbb63d740ce0828558d26e0ee4b2e7
aldeig <alkin.denis@gmail.com>
2024-04-10 12:34:36 +0400

    feat: добавил текст для неподписанных пользователей

--- app/src/dialog/handlers/tracking.py
+++ app/src/dialog/handlers/tracking.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Router, F
 from aiogram.types import CallbackQuery, Message
 
-from app.src.services.texts.tracking import SUBSCRIBE_CHANNEL
+from app.src.services.texts.user import UNSUBSCRIBE
 from app.src.services.user import (
     change_notify_state,
     check_subscribe_channel,
@@ -17,7 +17,7 @@ router = Router()
 async def get_query(msg: Message, bot: Bot, text: str):
     """Хендлер получает запрос на показ позици"""
     if not await check_subscribe_channel(msg.chat.id, bot):
-        await msg.answer(SUBSCRIBE_CHANNEL)
+        await msg.answer(UNSUBSCRIBE)
         return
     text, kb = await new_query(msg.chat.id, text)
     await msg.answer(text, reply_markup=kb)
@@ -29,7 +29,7 @@ async def get_query(msg: Message, bot: Bot, text: str):
 async def btn_update(call: CallbackQuery, bot: Bot, data: str, message: Message):
     await call.answer(cache_time=30)
     if not await check_subscribe_channel(message.chat.id, bot):
-        await message.answer(SUBSCRIBE_CHANNEL)
+        await message.answer(UNSUBSCRIBE)
         return
     _, track_id = data.split(":")
     text, kb = await update_query_positions(int(track_id))
@@ -45,7 +45,7 @@ async def btn_change_notify_state(
     call: CallbackQuery, bot: Bot, data: str, msg: Message
 ):
     if not await check_subscribe_channel(msg.chat.id, bot):
-        await msg.answer(SUBSCRIBE_CHANNEL)
+        await msg.answer(UNSUBSCRIBE)
         return
     action, track_id = data.split(":")
     await change_notify_state(int(track_id), action == "add")
